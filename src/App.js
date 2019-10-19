12b29245039cb8d1dbba318cb911b468f7e74919
Memo <p.szopian@studenti.unisa.it>
2019-10-19 19:56:04 +0200

    Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import ItemsList from './Components/ItemsList';
 import './App.css';
 
 class App extends Component {
   render(){
-    let names = ['Memo', 'Giovanni']
+    let names = ['Memo', 'Giovanni', 'Piercarlo']
     return (
-        <div className="App">
-          <header>
-            {names.map( name => <p>Ciao {name}</p>)}
-          </header>
+        <div>
+            <ItemsList items = {names}></ItemsList>
         </div>
     );
   }
