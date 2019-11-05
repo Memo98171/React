3fe99e20390fd6c3d82aac9cbfbe9235745284ee
Memo <p.szopian@studenti.unisa.it>
2019-11-05 09:00:17 +0100

    Pelo pelo pt 3

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 class Footer extends Component {
     render() {
         return (
